61b59e41e39057e4d0e2a23e81fb26037f0915b9
paulhemingway <phemingway@ymail.com>
2022-07-14 21:31:04 -0500

    login authentication, error message, validation

--- src/component/Login.js
+++ src/component/Login.js
@@ -8,27 +8,64 @@ import { signInWithEmailAndPassword } from "firebase/auth"
 export default function Login(props) {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
+  const [authError, setAuthError] = useState('')
+  const [error, setError] = useState(false)
 
-  
+  const handleKeyPress = (e) => {
+    if(e.key === 'Enter'){
+      login()
+    }
+  }
 
   const emailChange = (e) => {
-    console.log(e.target.value)
+    setEmail(e.target.value)
   }
 
   const passwordChange = (e) => {
-    console.log(e.target.value)
+    setPassword(e.target.value)
+  }
+
+  const valid = () => {
+    // validate email 
+    var emailValid = email.toLowerCase().match(/^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i)
+    
+    if(email.length === 0){
+      setAuthError("Validation Error: Please enter an email!")
+      setError(true)
+      return false
+    }
+
+    if(!emailValid){
+      setAuthError("Validation Error: Invalid email!")
+      setError(true)
+      return false
+    } 
+
+    if(password.length === 0){
+      setAuthError("Validation Error: Please enter a password!")
+      setError(true)
+      return false
+    }
+
+    return true
+
+
   }
 
   const login = async () => {
+    if(!valid()) return 
     try {
       const user = await signInWithEmailAndPassword(
         auth,
-        "phemingway@ymail.com",
-        "password"
+        email,
+        password
       );
 
     } catch (err) {
-      console.error(err.code)
+      // get rid of auth/ at the beggining and capitalize the first letter
+      var msg = err.code.substring(5).replace('-', ' ')
+      msg = msg.charAt(0).toUpperCase() + msg.slice(1)
+      setAuthError("Authentication Failed: " + msg)
     }
   }
 
@@ -43,12 +80,14 @@ export default function Login(props) {
             <div className="inputs">
               
               <div className="email">
-                <input type="email" name="email" id="email" placeholder="email" onChange={emailChange} required></input>
+                <input type="text" name="email" id="email" placeholder="email" onChange={emailChange} onKeyPress={handleKeyPress} required></input>
               </div>
               <div className="password">
-                <input type="password" name="username" id="username" placeholder="password" onChange={passwordChange} required></input>
+                <input type="password" name="username" id="username" placeholder="password" onChange={passwordChange} onKeyPress={handleKeyPress} required></input>
               </div>
+              {error && <p className='error-msg'>{authError}</p>}
             </div>
+            
             <div className="btn-container">
               <button onClick={login} className="login-btn" >SIGN IN</button>
             </div>
